afa2c03de83826fd62f7dc11f6e516380d04c610
Carlos Vier <carlos.vier@LAMEF.ufrgs.br>
2026-05-28 15:28:08 -0300

    ajustes para funcionamento no visual studio

--- Projeto/Estruturas/estruturas.cpp
+++ Projeto/Estruturas/estruturas.cpp
@@ -5,6 +5,6 @@ infoTela tela = { 1500, 800, "Hollow Knight",{ 0 }};
 
 infoPersonagem personagem = { { 10, 100 }, { 0 }, false}; // {0} limpa a struct da textura
 
-infoMapa map = { 151, 16, "Mapas/MapaTeste.txt", NULL };
+infoMapa map = { 151, 16, "Mapas/MapaTeste.txt", nullptr };
 
 infoBloco bloco = { 30.0f, 30.0f };
